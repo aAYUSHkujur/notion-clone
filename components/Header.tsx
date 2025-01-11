b320f78dea023093d22ad05db2ad20c5d28dfece
aayushk720@gmail.com <aayushk720@gmail.com>
2025-01-11 23:54:24 +0530

    header-updated

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,35 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { useUser } from "@clerk/nextjs";
+
 function Header() {
+  const { user } = useUser();
+
   return (
-    <div>Header</div>
-  )
+    <div className="flex items-center justify-between p-5">
+      {user && (
+        <h1 className="text-2xl">
+          {user?.firstName}
+          {`'s`} Space
+        </h1>
+      )}
+
+      {/* Breadcrumbs */}
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
 }
-export default Header
+export default Header;
