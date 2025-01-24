a4a52f8257d7a8facf90c3dc64baf6951ec97f02
aayushk720@gmail.com <aayushk720@gmail.com>
2025-01-24 22:32:26 +0530

    live cursor done

--- app/doc/[id]/layout.tsx
+++ app/doc/[id]/layout.tsx
@@ -1,15 +1,18 @@
 import RoomProvider from "@/components/RoomProvider";
+import React from "react";
 import { auth } from "@clerk/nextjs/server";
 
 function DocLayout({
   children,
-  params: { id },
+  params,
 }: {
   children: React.ReactNode;
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 }) {
+  const { id } = React.use(params); // Unwrap the Promise using React.use()
+
   auth.protect();
 
   return <RoomProvider roomId={id}>{children}</RoomProvider>;
